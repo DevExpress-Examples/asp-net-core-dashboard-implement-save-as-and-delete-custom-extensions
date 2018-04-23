3451c97b224fa2207d431c26b5d472506e03cd83
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/NetCoreWebApplication/Storages/CustomDashboardFileStorage.cs
@@ -0,0 +1,21 @@
+﻿using DevExpress.DashboardWeb;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreWebApplication.Storages
+{
+    public class CustomDashboardFileStorage : DashboardFileStorage {
+        public CustomDashboardFileStorage(string workingDirectory)
+            : base(workingDirectory) {
+        }
+
+        public void DeleteDashboard(string dashboardID) {
+            var dashboardPath = base.ResolveFileName(dashboardID);
+            if (File.Exists(dashboardPath))
+                File.Delete(dashboardPath);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASP.NET Core Dashboard - How to implement the Save As and Delete functionality by creating custom extensions
+
+
+This example is based on the code from <a href="https://www.devexpress.com/Support/Center/p/T504201">T504201: MVC Dashboard - How to implement the Save As and Delete functionality by creating custom extensions</a>. Refer to this article for more details.
+
+<br/>
+
+
